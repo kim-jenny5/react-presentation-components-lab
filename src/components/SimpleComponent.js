e3b52297ef0490ec64c051423c100d887d6111d0
Jenny Kim <kimj1905@gmail.com>
2021-11-08 19:25:42 -0500

    commit tree 5c3b701e51a0f55f9c80e1f3717fc78de6512785

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,17 @@
-// Code SimpleComponent Here
+import React, { Component } from "react";
+
+export default class SimpleComponent extends Component {
+  state = {
+    mood: "happy",
+  };
+
+  handleClick = (e) => {
+    // console.log(e.target);
+    const mood2 = this.state.mood === "happy" ? "sad" : "happy";
+    this.setState({ mood: mood2 });
+  };
+
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>;
+  }
+}
